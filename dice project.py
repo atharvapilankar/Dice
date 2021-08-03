d2cac229671b8db0549a317061f3f17e51b2f445
Abhir1902 <73550795+Abhir1902@users.noreply.github.com>
2021-08-03 08:15:49 +0530

    Update dice project.py

--- dice project.py
+++ dice project.py
@@ -2,7 +2,7 @@ import random
 print("Enter 1 is you want to roll the dice or press 0")
 n=int(input())
 while(n):
-    print(str(random.sample(range(1,6),1)))
+    print(str(random.sample(range(1,7),1)))
     print("Enter 1 if you want to roll the dice again or press 0")
     n=int(input())
 
