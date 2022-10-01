7c63f6d71ce8f04457843172471e70853e9615af
atharvapilankar <96884818+atharvapilankar@users.noreply.github.com>
2022-10-01 12:24:16 +0530

    changed print string

--- dice project.py
+++ dice project.py
@@ -1,5 +1,5 @@
 import random
-print("Enter 1 is you want to roll the dice or press 0")
+print("Enter 1 is you want to roll the dice or press 0 if you do not want to roll the dice.");
 n=int(input())
 while(n):
     print(str(random.sample(range(1,7),1)))
